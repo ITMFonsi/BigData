9a8df11cfe669770bd87a55d26d41d56737a98e1
Florian <fbe7236@students.fhv.at>
2018-10-01 12:05:59 +0200

    fixed what schatzi could not fix

--- aufgabe4master/aufgabe4master/src/main/java/at/fhv/itm2018/aufgabe4master/PiResultController.java
+++ aufgabe4master/aufgabe4master/src/main/java/at/fhv/itm2018/aufgabe4master/PiResultController.java
@@ -9,8 +9,7 @@ import java.util.LinkedList;
 
 @Controller
 public class PiResultController {
-
-    @RequestMapping(value = { "/getAllPiResults" }, method = RequestMethod.GET)
+    @RequestMapping(value = {"/welcome"}, method = RequestMethod.GET)
     public ModelAndView listAllPiResults() {
         LinkedList<String> list = new LinkedList<>();
         list.add("HELLO WORLD!");
