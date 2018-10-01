d70908de0196dedbd6e9bb78d2a9f6923bb8089d
tscholy <cts6638@students.fhv.at>
2018-10-01 14:02:15 +0200

    added servlet initializer

--- aufgabe4master/aufgabe4master/src/main/java/at/fhv/itm2018/aufgabe4master/Aufgabe4masterApplication.java
+++ aufgabe4master/aufgabe4master/src/main/java/at/fhv/itm2018/aufgabe4master/Aufgabe4masterApplication.java
@@ -2,11 +2,17 @@ package at.fhv.itm2018.aufgabe4master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Aufgabe4masterApplication {
+public class Aufgabe4masterApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aufgabe4masterApplication.class, args);
 	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Aufgabe4masterApplication.class);
+    }
 }
